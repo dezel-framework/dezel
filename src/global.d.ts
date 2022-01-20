d25ee1de5fcb2e65f7ee9dda83435baf920a0548
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2022-01-20 10:45:32 -0500

    Fixed typescript errors

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,24 +1,7 @@
-
-/**
- * @const global
- * @since 0.1.0
- */
-declare const global: any
-
-/**
- * @const _DEV_
- * @since 0.1.0
- */
-
 declare const _DEV_: boolean
 declare const _SIM_: boolean
-
-/**
- * @const $native
- * @since 0.1.0
- */
+declare const _TEST_: boolean
 declare const $native: any
-declare const console: any
 declare function importClass(name: string): any
 declare function importObject(name: string): any
 declare function requestAnimationFrame(callback: any): any
