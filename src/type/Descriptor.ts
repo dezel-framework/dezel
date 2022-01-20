998d4978c8ebb118dae2ea54eba63b3424b1fc85
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2022-01-20 10:46:27 -0500

    Added Descriptor type

--- src/type/Descriptor.ts
+++ src/type/Descriptor.ts
@@ -1,72 +1,22 @@
-import { Component } from 'component/Component'
+import { Body } from 'component/Body'
 import { View } from 'view/View'
 
 /**
- * @type TextDescriptor
+ * @const $node
  * @since 0.1.0
  */
-export type TextDescriptor = string | number
-
-/**
- * @type BoolDescriptor
- * @since 0.1.0
- */
-export type BoolDescriptor = boolean
-
-/**
- * @type NullDescriptor
- * @since 0.1.0
- */
-export type NullDescriptor = null | undefined
-
-/**
- * @type NodeDescriptor
- * @since 0.1.0
- */
-export type NodeDescriptor<T = any, D = any> = {
-	node?: any
-	type: any
-	data: any
-	children: Array<Descriptor<T, D>>
-}
+export const $node = Symbol('node')
 
 /**
  * @type Descriptor
  * @since 0.1.0
  */
-export type Descriptor<T = any, D = any> = NodeDescriptor<T, D> | TextDescriptor | BoolDescriptor | NullDescriptor
+export type Descriptor<T = any, D = any> = {
 
-/**
- * @function isTextDescriptor
- * @since 0.1.0
- */
-export function isTextDescriptor(descriptor: Descriptor): descriptor is TextDescriptor {
-	return (
-		typeof descriptor == 'string' ||
-		typeof descriptor == 'number'
-	)
-}
+	key?: any
+	type: any
+	attributes: any
+	children: Array<Descriptor<T, D>>
 
-/**
- * @function isBoolDescriptor
- * @since 0.1.0
- */
-export function isBoolDescriptor(descriptor: Descriptor): descriptor is BoolDescriptor {
-	return typeof descriptor == 'boolean'
-}
-
-/**
- * @function isBoolDescriptor
- * @since 0.1.0
- */
-export function isNullDescriptor(descriptor: Descriptor): descriptor is NullDescriptor {
-	return descriptor == null
-}
-
-/**
- * @function isNodeDescriptor
- * @since 0.1.0
- */
-export function isNodeDescriptor(descriptor: Descriptor): descriptor is NodeDescriptor {
-	return descriptor != null && descriptor != undefined && typeof descriptor == 'object'
+	[$node]?: any // Body | View | Slot
 }
