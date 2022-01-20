3237efce76ec3b01566675e70a541c0b16a0839d
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2022-01-20 10:48:42 -0500

    Removed temporarely

---
+++ src/view/test/Slot.test.ts
@@ -0,0 +1,20 @@
+import { $parent } from 'view/symbol/Slot'
+import { Application } from 'application/Application'
+import { Event } from 'event/Event'
+import { Slot } from 'view/Slot'
+import { View } from 'view/View'
+/*
+describe('Slot', () => {
+
+	let slot: Slot
+
+	beforeAll(() => {
+		Application.register()
+	})
+
+	beforeEach(() => {
+		slot = new Slot()
+	})
+
+})
+*/
