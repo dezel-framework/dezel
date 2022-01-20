a1b27490ff70c7cd0486dd52b41b2166aef75cbd
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2022-01-20 10:49:28 -0500

    Added static property

--- src/view/Window.ts
+++ src/view/Window.ts
@@ -1,3 +1,4 @@
+import { $main } from 'view/symbol/Window'
 import { bridge } from 'native/bridge'
 import { native } from 'native/native'
 import { View } from 'view/View'
@@ -10,6 +11,47 @@ import { View } from 'view/View'
  * @since 0.1.0
  */
 export class Window extends View {
+
+	//--------------------------------------------------------------------------
+	// Static Propertis
+	//--------------------------------------------------------------------------
+
+	/**
+	 * @property $main
+	 * @since 0.1.0
+	 * @hiden
+	 */
+	private static [$main]: Window | null = null
+
+	/**
+	 * The main window.
+	 * @property main
+	 * @since 0.1.0
+	 */
+	public static get main(): Window | null {
+		return this[$main]
+	}
+
+	//--------------------------------------------------------------------------
+	// Methods
+	//--------------------------------------------------------------------------
+
+	/**
+	 * Initialize the application.
+	 * @constructor
+	 * @since 0.1.0
+	 */
+	constructor() {
+
+		super()
+
+		let main = Window[$main]
+		if (main) {
+			main.destroy()
+		}
+
+		Window[$main] = this
+	}
 
 	/**
 	 * Finds the farthest view at a specified location.
